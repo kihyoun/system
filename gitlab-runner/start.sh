5ae2ac6f0889da223d5e6b28f481f0e4bd1a4ee4
Root <kihyoun@gmail.com>
2020-10-25 01:04:56 +0200

    refactor: add gitlab runner config for multi domain

--- gitlab-runner/start.sh
+++ gitlab-runner/start.sh
@@ -1,21 +1,23 @@
 #! /bin/bash
-source ../.docker.env
 
-docker-compose up --build --remove-orphans -d
+for i in ../.projects.env; do
+    docker-compose -p ${PROJECT_NAME} up --build --remove-orphans -d
 
-for i in $( seq 1 $GITLAB_RUNNER_DOCKER_SCALE )
-do
-    docker run --rm --volumes-from gitlab-runner_docker_$i gitlab/gitlab-runner register \
-    --non-interactive \
-    --executor "docker" \
-    --docker-image alpine:latest \
-    --url "$GITLAB_EXTERNAL_URL/" \
-    --registration-token "$GITLAB_RUNNER_TOKEN" \
-    --description "docker-$i" \
-    --tag-list "docker,aws" \
-    --run-untagged=true \
-    --locked=false \
-    --access-level="not_protected" \
-    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
-    --docker-privileged=true
+    for i in $( seq 1 $GITLAB_RUNNER_DOCKER_SCALE )
+    do
+        docker run --rm --volumes-from gitlab-runner_docker_$i gitlab/gitlab-runner register \
+        --non-interactive \
+        --executor "docker" \
+        --docker-image alpine:latest \
+        --url "$GITLAB_EXTERNAL_URL/" \
+        --registration-token "$GITLAB_RUNNER_TOKEN" \
+        --description "${PROJECT_NAME}-docker-$i" \
+        --tag-list "docker,aws" \
+        --run-untagged=true \
+        --locked=false \
+        --access-level="not_protected" \
+        --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+        --docker-privileged=true
+    done
+
 done
