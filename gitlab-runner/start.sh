77ddeaa886dc38649457c9d6a70b257764e4b3d9
Root <kihyoun@gmail.com>
2020-10-27 12:41:22 +0100

    fix: project name on compose  must be different

--- gitlab-runner/start.sh
+++ gitlab-runner/start.sh
@@ -4,17 +4,17 @@ source ../.docker.env
 for i in ../.projects.env/.*.env; do
     source $i
     export GITLAB_RUNNER_DOCKER_SCALE=$GITLAB_RUNNER_DOCKER_SCALE
-    docker-compose -p ${PROJECT_NAME} up --build --remove-orphans -d
+    docker-compose -p ${PROJECT_NAME}_runner up --build --remove-orphans -d
 
     for i in $( seq 1 $GITLAB_RUNNER_DOCKER_SCALE )
     do
-        docker run --rm --volumes-from ${PROJECT_NAME}_docker_$i gitlab/gitlab-runner register \
+        docker run --rm --volumes-from ${PROJECT_NAME}_runner_docker_$i gitlab/gitlab-runner register \
         --non-interactive \
         --executor "docker" \
         --docker-image alpine:latest \
         --url "$GITLAB_EXTERNAL_URL/" \
         --registration-token "$GITLAB_RUNNER_TOKEN" \
-        --description "$PROJECT_NAME-docker-$i" \
+        --description "$PROJECT_NAME-runner-docker-$i" \
         --tag-list "docker,aws" \
         --run-untagged=true \
         --locked=false \
