ccc2d46f00a2a7ce324d728bd086a51d8567cfc9
Root <kihyoun@gmail.com>
2020-10-29 22:44:02 +0100

    Gitlab Runner Local Domain Config

--- gitlab-runner/start.sh
+++ gitlab-runner/start.sh
@@ -4,12 +4,13 @@ source ../gitlab/.docker.env
 
 echo > ./.hosts
 
-[ $GITLAB_REGISTRY_DOMAIN_MODE -lt 2 ] && echo "${GITLAB_IP} ${GITLAB_HOST}" > ./.hosts
+[ $GITLAB_DOMAIN_MODE -lt 2 ] && echo "${GITLAB_IP} ${GITLAB_HOST}" >> ./.hosts
+[ $GITLAB_REGISTRY_DOMAIN_MODE -lt 2 ] && echo "${GITLAB_IP} ${GITLAB_REGISTRY_HOST}" >> ./.hosts
 
 for i in ../.projects.env/.*.env; do
     source $i
     export GITLAB_RUNNER_DOCKER_SCALE=$GITLAB_RUNNER_DOCKER_SCALE
-    
+
     docker-compose -p ${PROJECT_NAME}_runner up --build --remove-orphans -d
 
     for i in $( seq 1 $GITLAB_RUNNER_DOCKER_SCALE )
@@ -19,14 +20,16 @@ for i in ../.projects.env/.*.env; do
         --non-interactive \
         --executor "docker" \
         --docker-image alpine:latest \
-        --url "$GITLAB_EXTERNAL_URL/" \
+        --url $([ $GITLAB_DOMAIN_MODE -lt 2 ] && echo "http://$GITLAB_IP:$GITLAB_PORT/" || echo "${GITLAB_EXTERNAL_URL}") \
         --registration-token "$GITLAB_RUNNER_TOKEN" \
         --description "$PROJECT_NAME-runner-docker-$i" \
         --tag-list "docker,aws" \
         --run-untagged=true \
-        --locked=false \
         --access-level="not_protected" \
+        --docker-network-mode="gitlab_web" \
+        --clone-url $([ $GITLAB_DOMAIN_MODE -lt 2 ] && echo "http://$GITLAB_IP:$GITLAB_PORT/" || echo "${GITLAB_EXTERNAL_URL}") \
         --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+        $([ $GITLAB_DOMAIN_MODE -lt 2 ] && echo '--docker-volumes /etc/hosts:/etc/hosts') \
         --docker-privileged=true
     done
 
