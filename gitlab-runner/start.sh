81428d4bbb60fc027efbebcec5a760fba409ae64
Root <kihyoun@gmail.com>
2020-10-30 20:00:27 +0100

    add global gitlab runners

--- gitlab-runner/start.sh
+++ gitlab-runner/start.sh
@@ -5,6 +5,27 @@ export GITLAB_HOST=$GITLAB_HOST
 export GITLAB_REGISTRY_HOST=$GITLAB_REGISTRY_HOST
 export GITLAB_IP=$GITLAB_IP
 
+docker-compose -p runner up --build --remove-orphans -d
+
+for i in $( seq 1 $GITLAB_RUNNER_DOCKER_SCALE )
+do
+    docker run --rm --network=container:runner_docker_$i \
+    --volumes-from=runner_docker_$i gitlab/gitlab-runner register \
+    --non-interactive \
+    --executor "docker" \
+    --docker-image alpine:latest \
+    --url "http://$GITLAB_IP:$GITLAB_PORT/" \
+    --registration-token "$GITLAB_RUNNER_TOKEN" \
+    --description "runner-docker-$i" \
+    --tag-list "docker,aws" \
+    --run-untagged=true \
+    --access-level="not_protected" \
+    --clone-url "http://$GITLAB_IP:$GITLAB_PORT/" \
+    --docker-network-mode="gitlab_web" \
+    --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+    --docker-privileged=true
+done
+
 for i in ../.projects.env/.*.env; do
     source $i
     export GITLAB_RUNNER_DOCKER_SCALE=$GITLAB_RUNNER_DOCKER_SCALE
--- gitlab-runner/stop.sh
+++ gitlab-runner/stop.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 source ../.docker.env
 
+export GITLAB_RUNNER_DOCKER_SCALE=$GITLAB_RUNNER_DOCKER_SCALE
+
+for i in $( seq 1 $GITLAB_RUNNER_DOCKER_SCALE )
+do
+    docker run --rm --volumes-from ${PROJECT_NAME}_runner_docker_$i \
+        --network=container:${PROJECT_NAME}_runner_docker_$i \
+        gitlab/gitlab-runner unregister --name ${PROJECT_NAME}-runner-docker-$i \
+        --url ${GITLAB_EXTERNAL_URL} --all-runners
+done
+
+docker-compose -p ${PROJECT_NAME}_runner down
+
 for i in ../.projects.env/.*.env; do
     source $i
     export GITLAB_RUNNER_DOCKER_SCALE=$GITLAB_RUNNER_DOCKER_SCALE
