811aa46cc7e66ecdf35c71b172a1e803d70d6941
Ki-Hyoun Kim <kihyoun@gmail.com>
2020-11-06 16:49:42 +0100

    Create prepare-host.sh

---
+++ prepare-host.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install zsh git apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose --version
+
+# oh my
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo cat zshtmp > /root/.zshrc
+echo "READY"
+
