90b0c095f987cc318a95b5d0e9a32f2dc1a152e4
Ki-Hyoun Kim <kihyoun@gmail.com>
2020-11-07 02:08:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install zsh unzip git apt-transport-https \
+ apt-get update
+ apt-get upgrade -y
+ apt-get install zsh unzip git apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ apt-key fingerprint 0EBFCD88
+ add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-sudo docker-compose --version
+ apt-get update
+ apt-get install docker-ce docker-ce-cli containerd.io -y
+ curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ chmod +x /usr/local/bin/docker-compose
+ ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+ docker-compose --version
