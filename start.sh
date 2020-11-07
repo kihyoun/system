375eae8f9d51fe21cfe35030d7a858a8738edb06
Your Name <you@example.com>
2020-11-07 18:52:35 +0100

    fix: start scripts

--- start.sh
+++ start.sh
@@ -3,15 +3,15 @@ shopt -s dotglob
 while [ true ]; do
   if [ -f .docker.env ]; then
     source .docker.env
-    (cd gitlab; ./start.sh)
-    (cd nginx-proxy; ./start.sh)
-    (cd nginx; ./start.sh)
-    [ $ENABLE_SYNC = true ] && (cd sync; ./start.sh)
+    (cd gitlab; bash ./start.sh)
+    (cd nginx-proxy; bash ./start.sh)
+    (cd nginx; bash ./start.sh)
+    [ $ENABLE_SYNC = true ] && (cd sync; bash ./start.sh)
     [ $ENABLE_SYNC = false ] && ./wait-for-it.sh 127.0.0.1:8071 -t 0
-    ./stop.sh
+    bash ./stop.sh
   else
     echo "No .docker.env found. Starting synchronisation server..."
-    (cd sync; ./start.sh)
+    (cd sync; bash ./start.sh)
   fi
 
 done
