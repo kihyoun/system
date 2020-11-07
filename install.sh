5786f63b244a2efbef0477207c26a08f770dc737
Ki-Hyoun Kim <kihyoun@gmail.com>
2020-11-07 02:10:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
     curl \
     gnupg-agent \
     software-properties-common -y
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
  apt-key fingerprint 0EBFCD88
  add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
