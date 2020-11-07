ef8b54a4839bb0f2feda6222a4239e9649a80622
Ki-Hyoun Kim <kihyoun@gmail.com>
2020-11-07 02:41:05 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,6 @@
 [ ! -d system ] && git clone https://github.com/kihyoun/system.git
 if [ -d system ]; then
         cd system;
-        ./start.sh
 else
         echo "System not found. Please create a .docker.env"
 fi
