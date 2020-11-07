3e2452cad7181f4e9cf6b8bc733e8aae76a9b882
Your Name <you@example.com>
2020-11-08 00:09:00 +0100

    add default sync route

--- sync/start.sh
+++ sync/start.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
-npm start
-
-cat ../nginx/.templates/default.conf.template| sed -e "s@\${PROXY_UPSTREAM}@sync@g" \
+source ../system/.docker.env
+if [ ! -f ../.docker.env ]; then
+    cat ../nginx/.templates/default.conf.template| sed -e "s@\${PROXY_UPSTREAM}@sync@g" \
       -e "s@\${PROXY_IP}@$BOOTSTRAPPER_IP@g" \
       -e "s@\${PROXY_PORT}@8071@g" \
       -e "s@\${PROXY_HOSTNAME}@$BOOTSTRAPPER_IP@g" > /synctemplates/default.conf.template
 
-docker-compose -f ../system/docker-compose.yml up --build --remove-orphans --force-recreate -d sync
+    docker-compose -f ../system/docker-compose.yml up --build --remove-orphans --force-recreate -d sync
+fi
+
+npm start
+
+[ -f ../.docker.env ] && docker-compose -f ../system/docker-compose.yml down sync
