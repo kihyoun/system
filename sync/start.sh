fc475394ae714b9357020392381c63c243b2fb0e
Your Name <you@example.com>
2020-11-07 23:58:48 +0100

    create default route

--- sync/start.sh
+++ sync/start.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 npm start
+
+cat ../nginx/.templates/default.conf.template| sed -e "s@\${PROXY_UPSTREAM}@sync@g" \
+      -e "s@\${PROXY_IP}@$BOOTSTRAPPER_IP@g" \
+      -e "s@\${PROXY_PORT}@8071@g" \
+      -e "s@\${PROXY_HOSTNAME}@$BOOTSTRAPPER_IP@g" > /synctemplates/default.conf.template
+
+docker-compose -f ../system/docker-compose.yml up --build --remove-orphans --force-recreate -d sync
