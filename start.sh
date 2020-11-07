8b75bde03c7772335962e51280586d3141a923d4
Your Name <you@example.com>
2020-11-08 00:29:07 +0100

    fix default route

--- start.sh
+++ start.sh
@@ -19,6 +19,11 @@ while [ true ]; do
     bash ./stop.sh
   else
     echo "No .docker.env found. Starting synchronisation server..."
+    cat ../nginx/.templates/default.conf.template| sed -e "s@\${PROXY_UPSTREAM}@sync@g" \
+      -e "s@\${PROXY_IP}@$BOOTSTRAPPER_IP@g" \
+      -e "s@\${PROXY_PORT}@8071@g" \
+      -e "s@\${PROXY_HOSTNAME}@$BOOTSTRAPPER_IP@g" > /synctemplates/default.conf.template
+
     docker-compose -f system/docker-compose.yml up --build --remove-orphans --force-recreate -d sync
     (cd sync; bash ./start.sh)
     docker-compose -f system/docker-compose.yml down sync
