b4ebe4c326f09e7870164e2c5b7c16c1b164368b
Your Name <you@example.com>
2020-11-08 14:54:12 +0100

    remove GITLAB_HOME configuration option

--- gitlab/start.sh
+++ gitlab/start.sh
@@ -6,13 +6,13 @@ docker-compose -f ../system/docker-compose.yml up --build --remove-orphans -d gi
 printf "export GITLAB_IP=" > .docker.env
 docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' system_gitlab_1 >> .docker.env
 source .docker.env
-mkdir -p $GITLAB_HOME/config
+mkdir -p /srv/gitlab/config
 cat gitlab.rb \
     | sed -e "s@\${GITLAB_REGISTRY_URL}@${GITLAB_REGISTRY_URL}@g" \
     -e "s@\${GITLAB_REGISTRY_HOST}@${GITLAB_REGISTRY_HOST}@g" \
     -e "s@\${GITLAB_EXTERNAL_URL}@${GITLAB_EXTERNAL_URL}@g" \
     -e "s@\${GITLAB_REGISTRY_PORT}@${GITLAB_REGISTRY_PORT}@g" \
-    -e "s@\${GITLAB_IP}@${GITLAB_IP}@g" > $GITLAB_HOME/config/gitlab.rb
+    -e "s@\${GITLAB_IP}@${GITLAB_IP}@g" > /srv/gitlab/config/gitlab.rb
 
 docker restart system_gitlab_1
 
