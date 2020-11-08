1a30ae10d732d8f0717c0576fcf766d15b884c9f
Your Name <you@example.com>
2020-11-08 23:09:02 +0100

    add cors header

--- sync/src/index.ts
+++ sync/src/index.ts
@@ -20,6 +20,11 @@ app.use(cors({
   "preflightContinue": false,
   "optionsSuccessStatus": 204
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload({createParentPath: true}));
 app.use(morgan('combined'))
