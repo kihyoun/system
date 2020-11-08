de41c5f52632305a35e2e1bc8e0005b4c14c82a6
Your Name <you@example.com>
2020-11-08 01:11:48 +0100

    move gitlab to stop/start

--- system/seed.sh
+++ system/seed.sh
@@ -3,6 +3,10 @@
 if [ -f ../.docker.env ]; then
     source ../.docker.env
 else
+    source ../.docker.env.example
+    export REVIEW_SCALE=0
+    export PROD_SCALE=0
+    export BETA_SCALE=0
     export LIVEDIR=/srv
     export SSL_BASEDIR=/etc/letsencrypt
     [ -z $BACKUPDIR ] && export BACKUPDIR=/mnt/backup
