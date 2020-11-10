f6c5b843c52d8ee13a3871f517b584d025473205
Your Name <you@example.com>
2020-11-10 18:07:30 +0100

    fix: paths

--- stop.sh
+++ stop.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
-source .docker.env
+if [ -f .docker.env ]; then
+    source .docker.env
+else
+    source .docker.env.example
+    export REVIEW_SCALE=0
+    export PROD_SCALE=0
+    export BETA_SCALE=0
+    export SSL_BASEDIR=/etc/letsencrypt
+    [ -z $BACKUPDIR ] && export BACKUPDIR=/mnt/backup
+    [ ! -z $BACKUPDIR ] && export BACKUPDIR=$BACKUPDIR
+fi
+
 (cd gitlab-runner; bash stop.sh)
 (cd nginx-proxy; bash stop.sh)
 (cd gitlab; bash stop.sh)
