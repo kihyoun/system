c70c8bfdae2a70de37b4da45f3c3d2e35c18cdd4
Your Name <you@example.com>
2020-11-11 19:45:59 +0100

    fix: multiple proxies config

--- nginx-proxy/start.sh
+++ nginx-proxy/start.sh
@@ -4,26 +4,24 @@ for i in $(find ../.projects.env ../projects.env -type f -name "*.env" 2>/dev/nu
     ENV_FILE=./.projects/.$PROJECT_NAME.env
     echo > $ENV_FILE
 
-    [ $USE_BETA_HOST = true ] && export BETA_SCALE=1 || export BETA_SCALE=0;
-    [ $USE_REVIEW_HOST = true ] && export REVIEW_SCALE=1 || export REVIEW_SCALE=0;
-    [ $USE_PROD_HOST = true ] && export PROD_SCALE=1 || export PROD_SCALE=0;
-
-    docker-compose -p ${PROJECT_NAME} up --remove-orphans --build -d
-
-    if [ $USE_PROD_HOST = true ]; then
-        printf "PROD_PROXY=" >> $ENV_FILE
-        docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PROJECT_NAME}_prod_1 >> $ENV_FILE
-    fi
-
     if [ $USE_BETA_HOST = true ]; then
+        docker-compose -p ${PROJECT_NAME} up --remove-orphans --build -d beta
         printf "BETA_PROXY=" >> $ENV_FILE
         docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PROJECT_NAME}_beta_1 >> $ENV_FILE
     fi
 
+    if [ $USE_PROD_HOST = true ]; then
+        docker-compose -p ${PROJECT_NAME} up --remove-orphans --build -d prod
+        printf "PROD_PROXY=" >> $ENV_FILE
+        docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PROJECT_NAME}_prod_1 >> $ENV_FILE
+    fi
+
     if [ $USE_REVIEW_HOST = true ]; then
+        docker-compose -p ${PROJECT_NAME} up --remove-orphans --build -d review
         printf "REVIEW_PROXY=" >> $ENV_FILE
         docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PROJECT_NAME}_review_1 >> $ENV_FILE
     fi
+
 
  done
 
