7da48c561580905a01071b37ff084b13e389195b
Ki-Hyoun <kihyoun@gmail.com>
2020-11-12 02:49:39 +0100

    add snapd certbot to default install

--- bootstrapper/install.sh
+++ bootstrapper/install.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
- apt-get update
- apt-get upgrade -y
- apt-get install zsh unzip git apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
- apt-key fingerprint 0EBFCD88
- add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
- apt-get update
- apt-get install docker-ce docker-ce-cli containerd.io -y
- curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
- chmod +x /usr/local/bin/docker-compose
- ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
- docker-compose --version
+apt-get update
+apt-get upgrade -y
+apt-get install zsh unzip git apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io snapd -y
+# (optional) Install certbot
+snap install core; snap refresh core
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+# Dockekr Compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
