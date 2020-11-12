0b2c241eb30c87f840366bc95755fb77e0763dc9
Ki-Hyoun <kihyoun@gmail.com>
2020-11-12 01:23:39 +0100

    fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ -f system/.docker.env ]; then
     source system/.docker.env
 else
     source system/.docker.env.example
-    source system/.seen.env
+    source system/.seed.env
 fi
 
 docker-compose -f system/docker-compose.yml up --build --force-recreate -d bootstrapper
