203d3c9a44952b0f172e30b640f1b119b019e3f7
Ki-Hyoun <kihyoun@gmail.com>
2020-11-12 01:53:45 +0100

    typo

--- system/push.sh
+++ system/push.sh
@@ -7,7 +7,7 @@ if [ ! -f ../bootstrapper.zip ]; then
 	exit 1
 fi
 
-[ -f ../bootstrapper.zip ] && unzip -o ../bootstrapper.zip -d ./tmp/bootstrapper/
+[ -f ../bootstrapper.zip ] && unzip -o ../bootstrapper.zip -d /tmp/bootstrapper/
 [ -d /tmp/bootstrapper ] && rsync -av --progress --delete /tmp/bootstrapper/.projects.env/ .projects.env
 [ -d /tmp/bootstrapper ] && cp /tmp/bootstrapper/.*.env .
 [ -d /tmp/bootstrapper ] && cp /tmp/bootstrapper/*.env .
