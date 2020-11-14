1ce5b57f5e6b6aa640e2717af2dac07a2b323774
Ki-Hyoun <kihyoun@gmail.com>
2020-11-14 23:36:58 +0100

    fix: failing to stop sync

--- sync/stop.sh
+++ sync/stop.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
-
-if [ ! -f ../system/.docker.env ]; then
-  source ../system/.seed.env
-  source ../.docker.env
-  source ../wizard/.docker.env
-  docker-compose -f ../system/docker-compose.yml stop sync
-fi
+source ../system/.seed.env
+source ../.docker.env
+source ../wizard/.docker.env
+docker-compose -f ../system/docker-compose.yml stop sync
