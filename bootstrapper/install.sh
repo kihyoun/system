4cd4a48c6bed31f33937c639a8652e424b9b6e2a
Ki-Hyoun <kihyoun@gmail.com>
2020-11-14 23:05:03 +0100

    chore: remove snapd from base image

--- bootstrapper/install.sh
+++ bootstrapper/install.sh
@@ -14,7 +14,7 @@ add-apt-repository \
   $(lsb_release -cs) \
   stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io snapd -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
 # Dockekr Compose
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
