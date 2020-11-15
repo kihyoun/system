6e5d3a83b4225cf53a344cb9e0ef650652d6c97c
Ki-Hyoun <kihyoun@gmail.com>
2020-11-15 02:28:18 +0100

    fix: stop failure web

--- nginx/stop.sh
+++ nginx/stop.sh
@@ -4,6 +4,9 @@ if [ -f ../system/.docker.env ] && [ -f ../gitlab/.docker.env ]; then
     source ../gitlab/.docker.env
     docker-compose -f ../system/docker-compose.yml stop web
 else
-    echo "nginx [stop] skipped (no main config)"
+    echo "nginx [stop] no main config found. Trying to stop..."
+    source ../system/.seed.env
+    source ../.docker.env
+    docker-compose -f ../system/docker-compose.yml stop web
 fi
 
