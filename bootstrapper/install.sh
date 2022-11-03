d25d85cea1b121a443bd8ae740b2d8a0f8ef9e31
kihyoun <kihyoun@gmail.com>
2022-11-03 12:45:30 +0000

    update docker-compose version

--- bootstrapper/install.sh
+++ bootstrapper/install.sh
@@ -16,7 +16,7 @@ add-apt-repository \
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 # Dockekr Compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
